f024be8e064ff035c82142961fea27b2cc7a6323
James Heinrich <info@silisoftware.com>
2013-10-08 20:35:58 -0500

    remove unwanted break

--- phpthumb.class.php
+++ phpthumb.class.php
@@ -4046,7 +4046,6 @@ exit;
 							$this->DebugMessage('deleting "'.$tempfilename.'"', __FILE__, __LINE__);
 							unlink($tempfilename);
 							return $gdimg_source;
-							break;
 						} else {
 							$ErrorMessage = 'Failed to open tempfile in '.__FILE__.' on line '.__LINE__;
 							$this->DebugMessage($ErrorMessage, __FILE__, __LINE__);
