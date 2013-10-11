f3591675b41a0cb4b10c991b94fb401489c74f8d
James Heinrich <info@silisoftware.com>
2013-10-11 12:22:19 -0500

    [bugfix #925] custom URL separator won't work

--- phpthumb.class.php
+++ phpthumb.class.php
@@ -138,6 +138,7 @@ class phpthumb {
 	// * Security
 	var $config_high_security_enabled                = false;
 	var $config_high_security_password               = null;
+	var $config_high_security_url_separator          = '&';
 	var $config_disable_debug                        = true;
 	var $config_allow_src_above_docroot              = false;
 	var $config_allow_src_above_phpthumb             = true;
