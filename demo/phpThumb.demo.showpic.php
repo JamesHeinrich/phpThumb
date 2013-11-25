06cff8a8600b1ec5dfc1fa4d9c6b2c6364490c87
James Heinrich <info@silisoftware.com>
2013-11-25 14:53:46 -0600

    use phpThumbURL

--- demo/phpThumb.demo.showpic.php
+++ demo/phpThumb.demo.showpic.php
@@ -30,7 +30,7 @@
 
 die('For security reasons, this demo is disabled by default. Please comment out line '.__LINE__.' in '.basename(__FILE__));
 
-$phpThumbLocation = '../phpThumb.php?';
+$phpThumbLocation = '../phpThumb.php';
 
 echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
 echo '<html><head>';
@@ -42,74 +42,76 @@ if (isset($_GET['title'])) {
 }
 ?>
 
-	<script type="text/javascript">
-	<!--
-	// http://www.xs4all.nl/~ppk/js/winprop.html
-	function CrossBrowserResizeInnerWindowTo(newWidth, newHeight) {
-		if (self.innerWidth) {
-			frameWidth  = self.innerWidth;
-			frameHeight = self.innerHeight;
-		} else if (document.documentElement && document.documentElement.clientWidth) {
-			frameWidth  = document.documentElement.clientWidth;
-			frameHeight = document.documentElement.clientHeight;
-		} else if (document.body) {
-			frameWidth  = document.body.clientWidth;
-			frameHeight = document.body.clientHeight;
-		} else {
-			return false;
-		}
-		if (document.layers) {
-			newWidth  -= (parent.outerWidth - parent.innerWidth);
-			newHeight -= (parent.outerHeight - parent.innerHeight);
-		}
-
-		// original code:
-		//parent.window.resizeTo(newWidth, newHeight);
-		// fixed code: James Heinrich, 20 Feb 2004
-		parent.window.resizeBy(newWidth - frameWidth, newHeight - frameHeight);
-
-		return true;
+<script type="text/javascript">
+<!--
+// http://www.xs4all.nl/~ppk/js/winprop.html
+function CrossBrowserResizeInnerWindowTo(newWidth, newHeight) {
+	if (self.innerWidth) {
+		frameWidth  = self.innerWidth;
+		frameHeight = self.innerHeight;
+	} else if (document.documentElement && document.documentElement.clientWidth) {
+		frameWidth  = document.documentElement.clientWidth;
+		frameHeight = document.documentElement.clientHeight;
+	} else if (document.body) {
+		frameWidth  = document.body.clientWidth;
+		frameHeight = document.body.clientHeight;
+	} else {
+		return false;
 	}
-	// -->
-	</script>
+	if (document.layers) {
+		newWidth  -= (parent.outerWidth - parent.innerWidth);
+		newHeight -= (parent.outerHeight - parent.innerHeight);
+	}
 
-	<script type="text/javascript" src="javascript_api.js"></script>
+	// original code:
+	//parent.window.resizeTo(newWidth, newHeight);
+	// fixed code: James Heinrich, 20 Feb 2004
+	parent.window.resizeBy(newWidth - frameWidth, newHeight - frameHeight);
+
+	return true;
+}
+// -->
+</script>
+
+<script type="text/javascript" src="javascript_api.js"></script>
 
 <?php
-	function SafeStripSlashes($string) {
-		return (get_magic_quotes_gpc() ? stripslashes($string) : $string);
-	}
+function SafeStripSlashes($string) {
+	return (get_magic_quotes_gpc() ? stripslashes($string) : $string);
+}
+require_once('../phpThumb.config.php');
 
-	$additionalparameters = array();
-	foreach ($_GET as $key => $value) {
-		if (is_array($value)) {
-			foreach ($value as $key2 => $value2) {
-				$additionalparameters[] = $key.'[]='.SafeStripSlashes($value2);
-			}
-		} else {
-			$additionalparameters[] = $key.'='.SafeStripSlashes($value);
+$additionalparameters = array();
+foreach ($_GET as $key => $value) {
+	if (is_array($value)) {
+		foreach ($value as $key2 => $value2) {
+			$additionalparameters[] = $key.'[]='.SafeStripSlashes($value2);
 		}
+	} else {
+		$additionalparameters[] = $key.'='.SafeStripSlashes($value);
 	}
-	$imagesrc = $phpThumbLocation.implode('&', $additionalparameters);
+}
+//$imagesrc = $phpThumbLocation.implode('&', $additionalparameters);
+$imagesrc = phpThumbURL(implode($PHPTHUMB_CONFIG['config_high_security_url_separator'], $additionalparameters), $phpThumbLocation);
 
-	echo '<script type="text/javascript">';
-	echo 'var ns4;';
-	echo 'var op5;';
-	echo 'function setBrowserWindowSizeToImage() {';
-	echo 	'if (!document.getElementById("imageimg")) { return false; }';
-	echo	'sniffBrowsers();';
-	echo 	'var imageW = getImageWidth("imageimg");';
-	echo 	'var imageH = getImageHeight("imageimg");';
-			// check for maximum dimensions to allow no-scrollbar window
-	echo 	'if (((screen.width * 1.1) > imageW) || ((screen.height * 1.1) > imageH)) {'."\n";
-				// screen is large enough to fit whole picture on screen with 10% margin
-	echo 		'CrossBrowserResizeInnerWindowTo(imageW, imageH);'."\n";
-	echo 	'} else {'."\n";
-				// image is too large for screen: add scrollbars by putting the image inside an IFRAME
-	echo 		'document.getElementById("showpicspan").innerHTML = "<iframe width=\"100%\" height=\"100%\" marginheight=\"0\" marginwidth=\"0\" frameborder=\"0\" scrolling=\"on\" src=\"'.$imagesrc.'\">Your browser does not support the IFRAME tag. Please use one that does (IE, Firefox, etc).<br><img src=\"'.$imagesrc.'\"><\/iframe>";';
-	echo 	'}'."\n";
-	echo '}';
-	echo '</script>';
+echo '<script type="text/javascript">';
+echo 'var ns4;';
+echo 'var op5;';
+echo 'function setBrowserWindowSizeToImage() {';
+echo 	'if (!document.getElementById("imageimg")) { return false; }';
+echo	'sniffBrowsers();';
+echo 	'var imageW = getImageWidth("imageimg");';
+echo 	'var imageH = getImageHeight("imageimg");';
+		// check for maximum dimensions to allow no-scrollbar window
+echo 	'if (((screen.width * 1.1) > imageW) || ((screen.height * 1.1) > imageH)) {'."\n";
+			// screen is large enough to fit whole picture on screen with 10% margin
+echo 		'CrossBrowserResizeInnerWindowTo(imageW, imageH);'."\n";
+echo 	'} else {'."\n";
+			// image is too large for screen: add scrollbars by putting the image inside an IFRAME
+echo 		'document.getElementById("showpicspan").innerHTML = "<iframe width=\"100%\" height=\"100%\" marginheight=\"0\" marginwidth=\"0\" frameborder=\"0\" scrolling=\"on\" src=\"'.$imagesrc.'\">Your browser does not support the IFRAME tag. Please use one that does (IE, Firefox, etc).<br><img src=\"'.$imagesrc.'\"><\/iframe>";';
+echo 	'}'."\n";
+echo '}';
+echo '</script>';
 ?>
 
 </head>
