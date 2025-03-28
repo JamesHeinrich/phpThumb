2ecbc4146eb30d911d3add3a8be7092ef965abc4
James Heinrich <info@getid3.org>
2025-03-28 11:06:29 -0400

    v1.7.23-202503281052

--- phpthumb.class.php
+++ phpthumb.class.php
@@ -265,7 +265,7 @@ class phpthumb {
 	public $issafemode       = null;
 	public $php_memory_limit = null;
 
-	public $phpthumb_version = '1.7.22-202408102120';
+	public $phpthumb_version = '1.7.23-202503281052';
 
 	//////////////////////////////////////////////////////////////////////
 
